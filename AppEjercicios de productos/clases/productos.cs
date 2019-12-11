55f992c3d8411a5d30fd68809f0eb655454db34c
marcos0801 <angulomarcos97@gmail.com>
2019-12-10 23:20:07 -0500

    Agregar archivos de proyecto.

---
+++ AppEjercicios de productos/clases/productos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEjercicios_de_productos.clases
+{
+    class productos
+    {
+        private string Producto{ get; set; }
+        private string Precio { get; set; }
+    private string Peso { get; set; }
+
+    }
+}
